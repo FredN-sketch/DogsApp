a21a33e7097d54f3f3cdb282e77dfa9860617452
Sarbast Hesso <sarbast.hesso@gmail.com>
2025-05-14 13:08:02 +0200

    Added DogsController

---
+++ DogsApp.Mvc/Controllers/DogsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DogsApp.Mvc.Controllers
+{
+    public class DogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
