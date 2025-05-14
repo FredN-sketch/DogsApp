472f9f71ca2dccc614f2f4ea126abe4c1ab02485
albertito1985 <anulo@live.se>
2025-05-14 13:45:54 +0200

    Basic delen completed

--- DogsApp.Mvc/Controllers/DogsController.cs
+++ DogsApp.Mvc/Controllers/DogsController.cs
@@ -1,4 +1,5 @@
-﻿using DogsApp.Mvc.Services;
+﻿using DogsApp.Mvc.Models;
+using DogsApp.Mvc.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DogsApp.Mvc.Controllers
@@ -12,6 +13,19 @@ namespace DogsApp.Mvc.Controllers
         {
             var model = dogService.GetAllDogs();
             return View(model);
+        }
+
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create(Dog dog)
+        {
+            dogService.AddDog(dog);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
